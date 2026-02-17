425e4fa57e8e2a3464295c82325f311464904253
ChristianSCubo <chriscubo@gmail.com>
2026-02-16 23:23:46 -0300

    STYLE: Estilização dos componentes de acessibilidade

--- frontend/app/(private)/products/_components/HeaderProducts.tsx
+++ frontend/app/(private)/products/_components/HeaderProducts.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import Image from "next/image";
+import teste from "@images/next.svg";
+
 export default function HeaderProducts() {
     return (
-        <header className="w-full h-23 self-stretch p-6 bg-(--cor-primaria) inline-flex justify-between items-center">
-            <div className="text-(--cor-texto-branco) text-sm font-normal">Logo</div>
-
+        <header className="w-full px-3 py-4 bg-(--cor-primaria) inline-flex justify-between items-center">
+            <Image src={teste} alt="Logo Innovation Brindes" width={84} height={24} className="text-(--cor-texto-branco) text-sm font-normal" />
             <div className="flex items-center gap-6">
-                <div className="text-(--cor-texto-branco) text-xl font-normal">Foto</div>
-
-                <div className="w-48 flex flex-col gap-1">
-                    <span className="text-(--cor-texto-branco) text-xl font-normal">Ana Carol Machado</span>
-
+                <div className="inline-flex justify-center items-center gap-3">
+                    <Image src={teste} alt="e-mail" width={24} height={48} className="justify-start text-cor-texto-branco text-xl font-normal" />
+                    <Image src={teste} alt="telefone" width={24} height={24} className="justify-start text-cor-texto-branco text-xl font-normal" />
+                    <Image src={teste} alt="foto" width={24} height={24} className="justify-start text-cor-texto-branco text-xl font-normal" />
+                </div>
+                <div className="flex flex-col gap-1">
+                    <span className="text-(--cor-texto-branco) text-base font-normal">Ana Carol Machado</span>
                     <span className="text-(--cor-texto-branco) text-sm font-normal">Quarta, 29/02/2020</span>
                 </div>
             </div>
--- frontend/app/components/ui/forms/LoginForm.tsx
+++ frontend/app/components/ui/forms/LoginForm.tsx
@@ -5,28 +5,28 @@ import Link from "next/link";
 import Toast from "../messages/Toast";
 import { toast } from "react-toastify";
 import handlerLogin from "@/app/actions/loginAction";
-
-// import Image from "next/image";
+import Image from "next/image";
+import teste from "@images/next.svg";
 
 export default function LoginForm() {
     const [state, formAction, isPending] = useActionState(handlerLogin, null);
 
-    // useEffect(() => {
-    //     if (state?.error) {
-    //         toast.error(state.message);
-    //     }
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [state?.error]);
+    useEffect(() => {
+        if (state?.error) {
+            toast.error(state.message);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [state?.error]);
 
     return (
         <>
-            <h1 className="text-[var(--cor-primaria)] text-center">Bem-vindo a Innovation Brindes</h1>
-            <article className="flex justify-center gap-4 grow bg-[var(--cor-primaria,#7fbc03)] rounded-[15px] px-4 py-6 pt-18 max-lg:px-4 max-md:px-8 max-md:py-10">
-                <form action={formAction} className="flex flex-col items-center justify-center gap-3 max-w-[380px] self-stretch grow">
+            <h1 className="text-(--cor-primaria) text-center text-5xl lg:text-7xl">Bem-vindo a Innovation Brindes</h1>
+            <article className="flex justify-center gap-4 grow bg-(--cor-primaria,#7fbc03) rounded-[15px] px-4 py-6 pt-18 max-lg:px-4 max-md:px-8 max-md:py-10">
+                <form action={formAction} className="flex flex-col items-center justify-center gap-3 max-w-95 self-stretch grow">
                     <div className="flex flex-col items-start gap-8 self-stretch max-md:gap-4">
                         <div className="flex flex-col items-start gap-5 self-stretch">
-                            <div className="relative flex items-start gap-3 rounded-[50px] bg-white px-4 py-1 box-border self-stretch max-md:px-3">
-                                {/* <Image src={} alt="Usuário" className="" /> */}
+                            <div className="relative flex items-center gap-3 rounded-[50px] bg-white px-4 py-1 box-border self-stretch max-md:px-3 div-campo">
+                                <Image src={teste} alt="Usuário" width={24} height={24} />
                                 <input
                                     type="text"
                                     name="user"
@@ -34,12 +34,11 @@ export default function LoginForm() {
                                     placeholder="Usuário"
                                     autoComplete="username"
                                     defaultValue={state?.values?.user}
-                                    className="w-full bg-transparent outline-none"
+                                    className="w-full bg-transparent lg:text-lg"
                                 />
                             </div>
-
-                            <div className="relative flex items-start gap-3 rounded-[50px] bg-white px-4 py-1 box-border self-stretch max-md:px-3">
-                                {/* <Image src={} alt="Senha" className="" /> */}
+                            <div className="relative flex items-center gap-3 rounded-[50px] bg-white px-4 py-1 box-border self-stretch max-md:px-3 div-campo">
+                                <Image src={teste} alt="Senha" width={24} height={24} />
                                 <input
                                     type="password"
                                     name="password"
@@ -47,27 +46,25 @@ export default function LoginForm() {
                                     placeholder="Senha"
                                     autoComplete="current-password"
                                     defaultValue={state?.values?.password}
-                                    className="w-full bg-transparent"
+                                    className="w-full bg-transparent lg:text-lg"
                                 />
                             </div>
                         </div>
-
                         <div className="self-stretch flex items-start justify-between gap-5 text-base text-white">
                             <div className="flex gap-3 items-center">
                                 <input type="checkbox" name="manter-logado" id="manter-logado" defaultChecked={false} />
-                                <label htmlFor="manter-logado">Manter logado</label>
+                                <label htmlFor="manter-logado" className="text-sm text-white lg:text-lg">
+                                    Manter logado
+                                </label>
                             </div>
-
-                            {/* Evite href="#" — isso causa mismatch */}
-                            <Link href="/forgot-password" className="relative">
+                            <Link href="#" className="text-sm text-white lg:text-lg">
                                 Esqueceu a senha
                             </Link>
                         </div>
                     </div>
-                    <input type="submit" disabled={isPending} className="btn-primary-white login-btn" value={isPending ? "Enviando..." : "Login"} />
+                    <input type="submit" disabled={isPending} className="btn-primary-white login-btn lg:text-lg" value={isPending ? "Enviando..." : "Login"} />
                 </form>
             </article>
-
             <Toast />
         </>
     );
